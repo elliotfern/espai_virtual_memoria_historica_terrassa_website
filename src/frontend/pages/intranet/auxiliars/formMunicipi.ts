e9bb00177d91d551196ed762fdf34ed3abfb4cbe
Elliot Fernandez <elliot@hispantic.com>
2025-06-06 13:25:05 +0100

    Solucionat codi duplicat al formulari de municipi

--- src/frontend/pages/intranet/auxiliars/formMunicipi.ts
+++ src/frontend/pages/intranet/auxiliars/formMunicipi.ts
@@ -9,6 +9,7 @@ interface Fitxa {
   status: string;
   message: string;
   id: number;
+  ciutat: string;
   comarca: number;
   provincia: number;
   comunitat: number;
@@ -22,103 +23,67 @@ interface ApiResponse<T> {
 }
 
 export async function formMunicipi(isUpdate: boolean, id?: number) {
-  if (isUpdate && id) {
+  const btn1 = document.getElementById('refreshButtonComarca');
+  const btn2 = document.getElementById('refreshButtonProvincia');
+  const btn3 = document.getElementById('refreshButtonComunitat');
+  const btn4 = document.getElementById('refreshButtonEstat');
+  const divTitol = document.getElementById('titolFormMunicipi') as HTMLDivElement;
+  const btnmunicipiForm = document.getElementById('btnFormMunicipi') as HTMLButtonElement;
+  const municipiForm = document.getElementById('municipiForm');
+
+  let data: Partial<Fitxa> = {
+    comarca: 0,
+    provincia: 0,
+    comunitat: 0,
+    estat: 0,
+  };
+
+  if (!btn1 || !btn2 || !btn3 || !btn4 || !divTitol || !btnmunicipiForm || !municipiForm) return;
+
+  if (id && isUpdate) {
     const response = await fetchDataGet<ApiResponse<Fitxa>>(API_URLS.GET.MUNICIPI_ID(id), true);
 
-    const btn1 = document.getElementById('refreshButtonComarca');
-    const btn2 = document.getElementById('refreshButtonProvincia');
-    const btn3 = document.getElementById('refreshButtonComunitat');
-    const btn4 = document.getElementById('refreshButtonEstat');
+    if (!response || !response.data) return;
+    data = response.data;
 
-    if (response && response.data && btn1 && btn2 && btn3 && btn4) {
-      const data = response.data;
+    divTitol.innerHTML = `<h2>Modificació municipi: ${data.ciutat}</h2>`;
 
-      const divTitol = document.getElementById('titolFormMunicipi') as HTMLDivElement;
-      if (divTitol) {
-        divTitol.innerHTML = `<h2>Modificació municipi: ${data.ciutat}</h2>`;
-      }
+    renderFormInputs(data);
 
-      await auxiliarSelect(data.comarca, 'comarques', 'comarca', 'comarca');
-      await auxiliarSelect(data.provincia, 'provincies', 'provincia', 'provincia');
-      await auxiliarSelect(data.comunitat, 'comunitats', 'comunitat', 'comunitat');
-      await auxiliarSelect(data.estat, 'estats', 'estat', 'estat');
+    btnmunicipiForm.textContent = 'Modificar dades';
 
-      btn1.addEventListener('click', function (event) {
-        event.preventDefault();
-        auxiliarSelect(data.comarca, 'comarques', 'comarca', 'comarca');
-      });
-      btn2.addEventListener('click', function (event) {
-        event.preventDefault();
-        auxiliarSelect(data.provincia, 'provincies', 'provincia', 'provincia');
-      });
-
-      btn3.addEventListener('click', function (event) {
-        event.preventDefault();
-        auxiliarSelect(data.comunitat, 'comunitats', 'comunitat', 'comunitat');
-      });
-
-      btn4.addEventListener('click', function (event) {
-        event.preventDefault();
-        auxiliarSelect(data.estat, 'estats', 'estat', 'estat');
-      });
-
-      renderFormInputs(data);
-
-      const btn = document.getElementById('btnFormMunicipi') as HTMLButtonElement;
-      if (btn) {
-        btn.textContent = 'Modificar dades';
-      }
-    }
-
-    // auxiliarSelect;
-    const municipiForm = document.getElementById('municipiForm');
-    if (municipiForm) {
-      municipiForm.addEventListener('submit', function (event) {
-        transmissioDadesDB(event, 'PUT', 'municipiForm', API_URLS.PUT.MUNICIPI);
-      });
-    }
+    municipiForm.addEventListener('submit', function (event) {
+      transmissioDadesDB(event, 'PUT', 'municipiForm', API_URLS.PUT.MUNICIPI);
+    });
   } else {
-    const btn1 = document.getElementById('refreshButtonComarca');
-    const btn2 = document.getElementById('refreshButtonProvincia');
-    const btn3 = document.getElementById('refreshButtonComunitat');
-    const btn4 = document.getElementById('refreshButtonEstat');
+    btnmunicipiForm.textContent = 'Inserir dades';
 
-    if (btn1 && btn2 && btn3 && btn4) {
-      const divTitol = document.getElementById('titolFormMunicipi') as HTMLDivElement;
-      if (divTitol) {
-        divTitol.innerHTML = `<h2>Creació nou municipi:</h2>`;
-      }
-
-      await auxiliarSelect(0, 'comarques', 'comarca', 'comarca');
-      await auxiliarSelect(0, 'provincies', 'provincia', 'provincia');
-      await auxiliarSelect(0, 'comunitats', 'comunitat', 'comunitat');
-      await auxiliarSelect(0, 'estats', 'estat', 'estat');
-
-      btn1.addEventListener('click', function (event) {
-        event.preventDefault();
-        auxiliarSelect(0, 'comarques', 'comarca', 'comarca');
-      });
-      btn2.addEventListener('click', function (event) {
-        event.preventDefault();
-        auxiliarSelect(0, 'provincies', 'provincia', 'provincia');
-      });
-
-      btn3.addEventListener('click', function (event) {
-        event.preventDefault();
-        auxiliarSelect(0, 'comunitats', 'comunitat', 'comunitat');
-      });
-
-      btn4.addEventListener('click', function (event) {
-        event.preventDefault();
-        auxiliarSelect(0, 'estats', 'estat', 'estat');
-      });
-
-      const municipiForm = document.getElementById('municipiForm');
-      if (municipiForm) {
-        municipiForm.addEventListener('submit', function (event) {
-          transmissioDadesDB(event, 'POST', 'municipiForm', API_URLS.POST.MUNICIPI, true);
-        });
-      }
-    }
+    municipiForm.addEventListener('submit', function (event) {
+      transmissioDadesDB(event, 'POST', 'municipiForm', API_URLS.POST.MUNICIPI, true);
+    });
   }
+
+  await auxiliarSelect(data.comarca ?? 0, 'comarques', 'comarca', 'comarca');
+  await auxiliarSelect(data.provincia ?? 0, 'provincies', 'provincia', 'provincia');
+  await auxiliarSelect(data.comunitat ?? 0, 'comunitats', 'comunitat', 'comunitat');
+  await auxiliarSelect(data.estat ?? 0, 'estats', 'estat', 'estat');
+
+  btn1.addEventListener('click', function (event) {
+    event.preventDefault();
+    auxiliarSelect(data.comarca ?? 0, 'comarques', 'comarca', 'comarca');
+  });
+  btn2.addEventListener('click', function (event) {
+    event.preventDefault();
+    auxiliarSelect(data.provincia ?? 0, 'provincies', 'provincia', 'provincia');
+  });
+
+  btn3.addEventListener('click', function (event) {
+    event.preventDefault();
+    auxiliarSelect(data.comunitat ?? 0, 'comunitats', 'comunitat', 'comunitat');
+  });
+
+  btn4.addEventListener('click', function (event) {
+    event.preventDefault();
+    auxiliarSelect(data.estat ?? 0, 'estats', 'estat', 'estat');
+  });
 }
